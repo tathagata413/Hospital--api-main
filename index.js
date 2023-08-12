a3cfba33fd63fca2396a514841de8d98f5686f2d
Tathagata Mitra <68333829+tathagata413@users.noreply.github.com>
2023-08-12 19:40:07 +0530

    Hospital api

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const port = process.env.PORT || 5000;
+const db = require('./config/mongoose');
+
+const passport = require('passport');
+const passportJWT = require('./config/passport_jwt_strategy');
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+  }));
+
+//redirecting routes
+app.use('/', require('./routes'));
+
+app.listen(port, function (err) {
+    if (err) { console.log('error'); return; }
+    
+    console.log(`server is running on ${port}`);
+});
